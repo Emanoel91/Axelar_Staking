9ef2afa92bda723a7f47824f0dff46ac285a2ee8
Eman Raz <121406187+Emanoel91@users.noreply.github.com>
2025-07-25 01:53:22 +0330

    Update Main_Dashboard.py

--- Main_Dashboard.py
+++ Main_Dashboard.py
@@ -31,7 +31,7 @@ conn = snowflake.connector.connect(
 
 # --- Time Frame & Period Selection ---
 start_date = st.date_input("Start Date", value=pd.to_datetime("2022-01-01"))
-end_date = st.date_input("End Date", value=pd.to_datetime("2025-06-01"))
+end_date = st.date_input("End Date", value=pd.to_datetime("2025-06-30"))
 
 # --- Query Functions ---------------------------------------------------------------------------------------
 # --- Row 1: Total Amounts Staked, Unstaked, and Net Staked ---
